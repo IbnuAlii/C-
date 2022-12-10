3288c437b4a381a0ef041035db67a469259f848b
Mohamed Ali Nor <114952970+IbnuAlii@users.noreply.github.com>
2022-12-10 22:11:56 +0300

    Delete 02 The Basics directory

--- 02 The Basics/03 Constants/constants.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    const double pi = 3.14;  // constant -> means values cannot be changed.
-    pi = 0; // Error
-    // const -> prevents the value to accidentally change.
-    std::cout << pi;
-}
--- 02 The Basics/09 Standard library/standardLibarary.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main() {
-    double result= pow(2,3); //power ->  2^3 = 8
-    cout<<"The power is " <<result;
-}
