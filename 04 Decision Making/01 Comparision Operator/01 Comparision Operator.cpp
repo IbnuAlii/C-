0a69971fc2e7e30fad2d0a97c68caae9797f3495
Mohamed Ali Nor <114952970+IbnuAlii@users.noreply.github.com>
2022-12-10 22:12:56 +0300

    Delete 04 Decision Making directory

--- 04 Decision Making/01 Comparision Operator/01 Comparision Operator.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int x = 10;
-    bool result = x > 5;
-    cout << result <<endl;  //resul = 1 which means  true
-    cout <<boolalpha <<result;  //resul = 1 which means  true
-    return 0;
-}
--- 04 Decision Making/06 Conditional Operators/conditionalOperators.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int sales = 11'000;
-    double commission = (sales >10'000) ? .1 : .05;
-    cout << commission;
-
-}
